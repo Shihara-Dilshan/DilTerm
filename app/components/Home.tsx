aefc91af8503044bfa6a9b345b20237dfbf26316
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-12-03 16:29:54 +0800

    sandbox and child process problem fixed

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes.json';
 import styles from './Home.css';
+//eslint-disable-next-line
+const { exec } = require("child_process");
 
-export default function Home(): JSX.Element {
-  return (
-    <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
-      <Link to={routes.COUNTER}>to Counter</Link>
-    </div>
-  );
+
+class Home extends React.Component {
+    test = (e: React.MouseEvent<HTMLButtonElement>): void => {
+        exec('ls -la'.trim(), (error, stdout, stderr) => console.log(stdout));
+    };
+
+    render(): JSX.Element {
+        return (
+            <div className={styles.container} data-tid="container">
+
+            </div>
+        );
+
+    }
+
 }
+
+export default Home;
+

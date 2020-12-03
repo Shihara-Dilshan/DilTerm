67cf770e680474eada22109069fd4fccfa8e9e60
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-12-03 18:28:55 +0800

    working

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,18 +1,58 @@
-import React from 'react';
+import React, { Component } from "react";
 import styles from './Home.css';
-//eslint-disable-next-line
 const { exec } = require("child_process");
 
 
-class Home extends React.Component {
+class Home extends Component<{}, { command: String, results: String[] }> {
+
+    constructor(props: Object){
+    super(props);
+    this.state = { command: "", results: [] };
+  }
+
     test = (e: React.MouseEvent<HTMLButtonElement>): void => {
-        exec('ls -la'.trim(), (error, stdout, stderr) => console.log(stdout));
+      
+       
     };
+    
+    execute = (e: React.ChangeEvent<HTMLInputElement>):void => {
+       let inputValue = e.target.value;
+       this.setState({ command: inputValue});
+    };
+    
+    finalFunc = (e: React.KeyboardEvent<FormControl>):void => {
+        if(e.key === "Enter"){
+            if(this.state.command.length !== 0){
+      exec(`${this.state.command}`.trim(), (error, stdout, stderr) => {
+      
+      let view:Array = stdout.split(`\n`);
+       this.setState({ results: view});
+      });      
+     
+      }
+        }
+    }
+    
+    styleLine = ():Object => {
+        return{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            overFlow: "auto"
+        }
+    }
 
     render(): JSX.Element {
+        const { command, results } = this.state;
         return (
-            <div className={styles.container} data-tid="container">
-
+          <div>
+            <div style={this.styleLine()}>
+            
+           <h3 style={{color:"red"}}>root@dilbash:~# </h3>
+           {" "}<input autoFocus style={{height: "2em", fontSize: "18px" ,marginLeft: "1%" ,width: "90%", color:"white", backgroundColor: "black", outLine: "none", border: "none"}} type="text" onChange={this.execute} value={command} onKeyUp={this.finalFunc}/>
+            </div>
+            {results.map((result,index)=> <h4 key={index}>{result}</h4>)}
+            
             </div>
         );
 

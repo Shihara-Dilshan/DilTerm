8346a52abf3f89b7eaf1673774fa138fc0a6a1d2
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-12-04 12:55:25 +0800

    readme added

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 const { exec } = require("child_process");
 
-
 class Input extends Component<{}, { command: String, results: String[], currentWD: String }> {
 
     constructor(props: Object) {
@@ -20,59 +19,68 @@ class Input extends Component<{}, { command: String, results: String[], currentW
     };
 
     finalFunc = (e: React.KeyboardEvent<FormControl>): void => {
-        
-        const options: Object = {
-           cwd: localStorage.getItem("cwd") === null ? this.state.currentWD : localStorage.getItem("cwd"),
-           env: null,
-           encording: 'utf-8',
-           timeout: 0,
-           maxBuffer: 200*1024,
-           killSignal: 'SIGTERM'
-        
-        };
-        
-        
-        if (e.key === "Enter") {
-            console.log(options.cwd);
-            if (this.state.command.length !== 0 && this.state.command !== "clear" && !this.state.command.includes("history")) {
-                exec(`${this.state.command}`.trim(), options ,(error, stdout, stderr) => {
-                    if(stdout){
-                       let view: Array = stdout.split(`\n`);
-                       this.setState({ results: view });   
-                       return;                  
-                    }else if(stderr){
-                       let view: Array = [];
-                       view.push(stderr);
-                       this.setState({ results: view });   
-                       return; 
-                    }else if(error){
-                       let view: Array = [];
-                       view.push(stderr);
-                       this.setState({ results: view }); 
-                       return; 
-                    }
-                    
-                 
-                    if(this.state.command === "cd ~" || this.state.command === "cd"){
-                    this.setState({currentWD: "/"});    
-                    localStorage.setItem("cwd", "/");
-                    return;
-                    }
-                    
-                    if(this.state.command.includes("cd")){
-                        let CCommand: String[] = this.state.command.split(" ");
-                    CCommand.shift();
 
-                    let currentCWD = this.state.currentWD;
-                    console.log(currentCWD);
-                    this.setState({currentWD: `${currentCWD}/${CCommand[0]}`});
-                    localStorage.setItem("cwd", this.state.currentWD);
-                    
-                    }
-                    
-                    
-                });
+        const options: Object = {
+            cwd: localStorage.getItem("cwd") === null ? this.state.currentWD : localStorage.getItem("cwd"),
+            env: null,
+            encording: 'utf-8',
+            timeout: 0,
+            maxBuffer: 200 * 1024,
+            killSignal: 'SIGTERM'
+
+        };
+
+
+        if (e.key === "Enter") {
+            e.target.setAttribute("disabled", true);
+            
+            //exit command
+            if(this.state.command === "exit"){
                 
+                return;
+            }
+            
+            
+
+            if (this.state.command.length !== 0 && this.state.command !== "clear" && !this.state.command.includes("history")) {
+                exec(`${this.state.command}`.trim(), options, (error, stdout, stderr) => {
+                    if (stdout) {
+                        let view: Array = stdout.split(`\n`);
+                        this.setState({ results: view });
+                        return;
+                    } else if (stderr) {
+                        let view: Array = [];
+                        view.push(stderr);
+                        this.setState({ results: view });
+                        return;
+                    } else if (error) {
+                        let view: Array = [];
+                        view.push(stderr);
+                        this.setState({ results: view });
+                        return;
+                    }
+
+
+                    if (this.state.command === "cd ~" || this.state.command === "cd") {
+                        this.setState({ currentWD: "/" });
+                        localStorage.setItem("cwd", "/");
+                        return;
+                    }
+
+                    if (this.state.command.includes("cd")) {
+                        let CCommand: String[] = this.state.command.split(" ");
+                        CCommand.shift();
+
+                        let currentCWD = this.state.currentWD;
+                        console.log(currentCWD);
+                        this.setState({ currentWD: `${currentCWD}/${CCommand[0]}` });
+                        localStorage.setItem("cwd", this.state.currentWD);
+
+                    }
+
+
+                });
+
 
             }
         }
@@ -98,7 +106,7 @@ class Input extends Component<{}, { command: String, results: String[], currentW
                     {" "}<input autoFocus style={{ height: "2em", fontSize: "15px", marginLeft: "1%", width: "90%", color: "white", backgroundColor: "#212121", outLine: "none", border: "none" }} type="text" onChange={this.execute} value={command} onKeyUp={this.finalFunc} />
                 </div>
                 {results.map((result, index) => <h4 key={index}>{result}</h4>)}
-		
+
             </div>
         );
 

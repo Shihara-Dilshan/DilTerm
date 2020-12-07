65aa7a8695d3c0ad14ec5a3efdecc3a4a70ebce8
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-12-07 11:17:05 +0800

    add ts ignore

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-const { remote } = require('electron')
-const { app,BrowserWindow } = remote
+
+const { remote } = require('electron');
+const { app } = remote;
 
 const { exec, execSync } = require('child_process');
 
@@ -14,19 +15,19 @@ class Input extends Component<{}, { command: string, results: string[], currentW
       reversedHistoryTemp: [],
       reversedHistory: [],
       currentWD: localStorage.getItem('cwd') === null ? '/' : localStorage.getItem('cwd'),
-      systemCommands: ['help',]
-    };  
+      systemCommands: ['help']
+    };
   }
-  
-  componentDidMount():void{
-      if(localStorage.getItem("history") === null){
-          localStorage.setItem("history", JSON.stringify(this.state.history));
-      }
-      
-       if(localStorage.getItem("historyTemp") === null){
-          localStorage.setItem("historyTemp", JSON.stringify(this.state.reversedHistoryTemp));
-      }
-      
+
+  componentDidMount(): void {
+    if (localStorage.getItem('history') === null) {
+      localStorage.setItem('history', JSON.stringify(this.state.history));
+    }
+
+    if (localStorage.getItem('historyTemp') === null) {
+      localStorage.setItem('historyTemp', JSON.stringify(this.state.reversedHistoryTemp));
+    }
+
   }
 
   execute = (e: React.ChangeEvent<HTMLInputElement>): void => {
@@ -35,22 +36,23 @@ class Input extends Component<{}, { command: string, results: string[], currentW
   };
 
   finalFunc = (e: React.KeyboardEvent<HTMLInputElement>): void => {
-    if(e.key === "Enter"){
-       this.setState({ reversedHistoryTemp : [] });
-       localStorage.setItem("historyTemp", JSON.stringify(this.state.reversedHistoryTemp));
+    if (e.key === 'Enter') {
+      this.setState({ reversedHistoryTemp: [] });
+      localStorage.setItem('historyTemp', JSON.stringify(this.state.reversedHistoryTemp));
     }
-    let currentHistory: Array<string> =  JSON.parse(localStorage.getItem("history"));
-    
-    if(e.key === "Enter" && this.state.command.length > 0){
-       if(currentHistory.length > 20){
-           currentHistory.shift();
-       }
-    
-    
-       currentHistory.push(this.state.command);
-       localStorage.setItem("history", JSON.stringify(currentHistory));
+    // @ts-ignore
+    let currentHistory: Array<string> = JSON.parse(localStorage.getItem('history'));
+
+    if (e.key === 'Enter' && this.state.command.length > 0) {
+      if (currentHistory.length > 20) {
+        currentHistory.shift();
+      }
+
+
+      currentHistory.push(this.state.command);
+      localStorage.setItem('history', JSON.stringify(currentHistory));
     }
-  
+
     const options: Object = {
       cwd: localStorage.getItem('cwd') === null ? this.state.currentWD : localStorage.getItem('cwd'),
       env: null,
@@ -59,47 +61,45 @@ class Input extends Component<{}, { command: string, results: string[], currentW
       maxBuffer: 200 * 1024,
       killSignal: 'SIGTERM'
     };
-    
-    if(e.key === "Enter" && this.state.command === "help")
-    {
-        exec("echo classified23 | sudo chmod +x help.sh | ./help.sh", (error, stdout, stderr) => {
-            let view: string[] = stdout.split(`\n`);
-            console.log(view)
-            this.setState({ results: view });
-        });
-        return;
-    }
 
-    
-    if(e.key === "Enter" && this.state.command === "about")
-    {
-        exec("echo classified23 | sudo chmod +x about.sh | ./about.sh", (error, stdout, stderr) => {
-            let view: string[] = stdout.split(`\n`);
-            console.log(view)
-            this.setState({ results: view });
-        });
-        return;
-    }
-
-    if(e.key === "Enter" && this.state.command === "exit")
-    {
-        app.quit();
-        return;
+    if (e.key === 'Enter' && this.state.command === 'help') {
+      // @ts-ignore
+      exec('echo classified23 | sudo chmod +x help.sh | ./help.sh', (error, stdout, stderr) => {
+        let view: string[] = stdout.split(`\n`);
+        console.log(view);
+        this.setState({ results: view });
+      });
+      return;
     }
 
 
-    if ((e.key === "Enter" && this.state.command.includes('exec')) || (e.key === "Enter" && this.state.command.includes('cd'))) {
-      e.target.setAttribute("disabled", true);
+    if (e.key === 'Enter' && this.state.command === 'about') {    // @ts-ignore
+      exec('echo classified23 | sudo chmod +x about.sh | ./about.sh', (error, stdout, stderr) => {
+        let view: string[] = stdout.split(`\n`);
+        console.log(view);
+        this.setState({ results: view });
+      });
+      return;
+    }
+
+    if (e.key === 'Enter' && this.state.command === 'exit') {
+      app.quit();
+      return;
+    }
+
+
+    if ((e.key === 'Enter' && this.state.command.includes('exec')) || (e.key === 'Enter' && this.state.command.includes('cd'))) {
+      // @ts-ignore
+      e.target.setAttribute('disabled', true);
       this.execAsync(this.state.command, options);
 
 
-    } else if (e.key === "Enter" && !this.state.command.includes('exec')) {
-      e.target.setAttribute("disabled", true);
+    } else if (e.key === 'Enter' && !this.state.command.includes('exec')) {
+      // @ts-ignore
+      e.target.setAttribute('disabled', true);
       this.execSync(this.state.command, options);
 
     }
-
-
 
 
   };
@@ -114,12 +114,12 @@ class Input extends Component<{}, { command: string, results: string[], currentW
       } catch (err) {
         let view: string[] = err.toString().split(`\n`);
         this.setState({ results: view });
-        console.log(err)
+        console.log(err);
       }
 
 
     }
-  }
+  };
 
   execAsync = (command: string, options: object): void => {
     exec(`${command}`.trim(), options, (error: any, stdout: string, stderr: string) => {
@@ -135,79 +135,82 @@ class Input extends Component<{}, { command: string, results: string[], currentW
         view.push(stderr);
         this.setState({ results: view });
       }
-      
+
       let regexForCd: object = /^[c][d][\s][../]{1,}$/;
+      // @ts-ignore
+      if (regexForCd.test(String(`${this.state.command}`).toLowerCase())) {
+        let modifiedArray: string[] = `${this.state.command}`.split('/');
+        modifiedArray = modifiedArray.filter(item => item.includes('..'));
 
-      if(regexForCd.test(String(`${this.state.command}`).toLowerCase())){
-          let modifiedArray: string[] = `${this.state.command}`.split("/");
-          modifiedArray = modifiedArray.filter(item => item.includes(".."));
-          
-          let local:any = localStorage.getItem("cwd");
-          local = local.split("/")
+        let local: any = localStorage.getItem('cwd');
+        local = local.split('/');
 
-          
-          if(local.filter( item => item !== "").length >= modifiedArray.length){
-            for(let  i=0; i < modifiedArray.length; i++){
-             let letPopValue:string = "/"+ local.pop();
-             let newValue = localStorage.getItem("cwd").replace(letPopValue, "");
-             localStorage.setItem("cwd", newValue)
-             }
-          }else{
-             this.setState({ currentWD: "/" });
-             localStorage.setItem("cwd", "/");
+        // @ts-ignore
+        if (local.filter(item => item !== '').length >= modifiedArray.length) {
+          for (let i = 0; i < modifiedArray.length; i++) {
+            let letPopValue: string = '/' + local.pop();
+            // @ts-ignore
+            let newValue = localStorage.getItem('cwd').replace(letPopValue, '');
+            localStorage.setItem('cwd', newValue);
           }
-          
-          return;
-      }
+        } else {
+          this.setState({ currentWD: '/' });
+          localStorage.setItem('cwd', '/');
+        }
 
-
-      if (command === "cd ~" || command === "cd") {
-        this.setState({ currentWD: "/" });
-        localStorage.setItem("cwd", "/");
         return;
       }
-      
-      
-     
 
-      if (command.includes("cd")) {
-        let CCommand: String[] = command.split(" ");
+
+      if (command === 'cd ~' || command === 'cd') {
+        this.setState({ currentWD: '/' });
+        localStorage.setItem('cwd', '/');
+        return;
+      }
+
+
+      if (command.includes('cd')) {
+        let CCommand: String[] = command.split(' ');
         CCommand.shift();
 
         let currentCWD = this.state.currentWD;
         console.log(currentCWD);
         this.setState({ currentWD: `${currentCWD}/${CCommand[0]}` });
-        localStorage.setItem("cwd", this.state.currentWD);
+        localStorage.setItem('cwd', this.state.currentWD);
         return;
       }
     });
-  }
-  
+  };
+  // @ts-ignore
   manageHistory = (e) => {
-      let currentHistoryTemp: Array<string> =  JSON.parse(localStorage.getItem("historyTemp"));
-      if(e.key === "Enter"){
-         e.preventDefault();
-      }else if(e.key === "ArrowUp"){
-         let currentHistory: Array<string> =  JSON.parse(localStorage.getItem("history"));
-         if(currentHistory.length === 0) return
-            
-         let popValue = currentHistory.pop();
-         currentHistory.unshift(popValue);
-         localStorage.setItem("history", JSON.stringify(currentHistory));
-         e.target.value = popValue;
-         
-         currentHistoryTemp.push(popValue);
-         localStorage.setItem("historyTemp", JSON.stringify(currentHistoryTemp));
-      }else if(e.key === "ArrowDown"){
-         if(currentHistoryTemp.length === 0) return
+    // @ts-ignore
+    let currentHistoryTemp: Array<string> = JSON.parse(localStorage.getItem('historyTemp'));
+    if (e.key === 'Enter') {
+      e.preventDefault();
+    } else if (e.key === 'ArrowUp') {
+      // @ts-ignore
+      let currentHistory: Array<string> = JSON.parse(localStorage.getItem('history'));
+      if (currentHistory.length === 0) return;
 
-         
-         let popValue = currentHistoryTemp.pop();
-          localStorage.setItem("historyTemp", JSON.stringify(currentHistoryTemp));
-          e.target.value = popValue;
-      }
-      
-  }
+      let popValue = currentHistory.pop();
+      // @ts-ignore
+      currentHistory.unshift(popValue);
+      localStorage.setItem('history', JSON.stringify(currentHistory));
+      e.target.value = popValue;
+
+      // @ts-ignore
+      currentHistoryTemp.push(popValue);
+      localStorage.setItem('historyTemp', JSON.stringify(currentHistoryTemp));
+    } else if (e.key === 'ArrowDown') {
+      if (currentHistoryTemp.length === 0) return;
+
+
+      let popValue = currentHistoryTemp.pop();
+      localStorage.setItem('historyTemp', JSON.stringify(currentHistoryTemp));
+      e.target.value = popValue;
+    }
+
+  };
 
   render(): JSX.Element {
     const { command, results } = this.state;
@@ -218,16 +221,17 @@ class Input extends Component<{}, { command: string, results: string[], currentW
 
           <h3 style={{ color: '#00c853' }}>root@DilTerm<span style={{ color: 'white' }}>:</span><span
             style={{ color: '#03a9f4' }}>{localStorage.getItem('cwd') !== null ? localStorage.getItem('cwd')?.replace('/', '') : '~'}</span><span
-              style={{ color: 'white' }}>$</span></h3>
+            style={{ color: 'white' }}>$</span></h3>
           {' '}<input autoFocus style={{
-            height: '2em',
-            fontSize: '15px',
-            marginLeft: '1%',
-            width: '90%',
-            color: 'white',
-            backgroundColor: '#212121',
-            border: 'none'
-          }} type="text" onChange={this.execute} value={command} onKeyUp={this.finalFunc} onKeyDown={this.manageHistory}/>
+          height: '2em',
+          fontSize: '15px',
+          marginLeft: '1%',
+          width: '90%',
+          color: 'white',
+          backgroundColor: '#212121',
+          border: 'none'
+        }} type="text" onChange={this.execute} value={command} onKeyUp={this.finalFunc}
+                      onKeyDown={this.manageHistory} />
         </div>
         {results.map((result, index) => <h4 key={index}>{result}</h4>)}
 
